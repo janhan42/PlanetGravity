9a422123bbd526e9459d0f6ab8fb1b89caaff841
janhan42 <wkdgns720@naver.com>
2023-12-20 20:50:30 +0900

    push

--- installer.sh
+++ installer.sh
@@ -1,3 +1,8 @@
 #!bin/bash
 
-git clone https://github.com/janhan42/Game/PlanetGravity
+git clone https://github.com/janhan42/PlanetGravity.git
+cd PlanetGravity
+make -C PlanetGravity bash -c "$(curl -fsSL https://raw.githubusercontent.com/janhan42/SFML-installer/master/installer.sh)"
+make -C PlanetGravity all
+
+make -C ./PlanetGravity
