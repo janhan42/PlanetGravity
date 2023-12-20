a3ba96df1d921ce56d4ec7b47b0fa4559861cbea
jang hun han <84340857+janhan42@users.noreply.github.com>
2023-12-20 23:55:50 +0900

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -3,5 +3,6 @@
 git clone https://github.com/janhan42/PlanetGravity.git
 cd PlanetGravity
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/janhan42/SFML-installer/master/installer.sh)"
+mkdir .OBJ
 make
 ./PlanetGravity
